a1bc8ac16ac6ffd9608ffcfd917faa77c6f1f42c
Surbhi Khengar <surbhikhengar25@gmail.com>
2026-06-17 09:33:49 +0530

    Created comment model

---
+++ Helpdesk or Ticketing System/helpdesk/src/models/Comment.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const commentSchema =
+  new mongoose.Schema(
+    {
+      ticket: {
+        type:
+          mongoose.Schema.Types.ObjectId,
+        ref: "Ticket",
+        required: true,
+      },
+
+      user: {
+        type:
+          mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+
+      message: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
+
+const Comment =
+  mongoose.model(
+    "Comment",
+    commentSchema
+  );
+
+export default Comment;
